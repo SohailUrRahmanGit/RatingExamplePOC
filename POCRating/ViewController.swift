bb398ec6ec3b6d359ec75bbb7d1bfc196304f324
Sohail-ur-rahman <ssohailmails@gmail.com>
2019-08-09 10:34:48 +0530

    Initial Commit

---
+++ POCRating/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  POCRating
+//
+//  Created by sohail ur rahman on 09/08/19.
+//  Copyright © 2019 sohail ur rahman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
